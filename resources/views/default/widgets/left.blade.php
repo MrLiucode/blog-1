825d6dae87b0c5d94eb203e3fe893a0fd6beace4
李立纯 <Fakeronline@users.noreply.github.com>
2015-07-23 23:31:27 +0800

    首页的完善

---
+++ resources/views/default/widgets/left.blade.php
@@ -0,0 +1,88 @@
+<ul class="bloglist">
+    <li>
+        <div class="ci"></div><!--圆形-->
+        <div class="row_box">
+            <div class="ti"></div><!--三角形-->
+
+            <h2 class="title"> <a href="#">我希望爱情是这样的</a></h2>
+            <ul class="textinfo">
+                <a href="#"><img src="images/s1.jpg"></a>
+                <p>我希望我的爱情是这样的，相濡以沫，举案齐眉，平淡如水。我在岁月中找到他，依靠他，将一生交付给他。做他的妻子，他孩子的母亲，为他做饭，洗衣服，缝一颗掉了的纽扣。然后，我们一起在时光中变老。</p>
+            </ul>
+            <ul class="details">
+                <li class="likes"><a href="#">10</a></li>
+                <li class="comments"><a href="#">34</a> </li>
+                <li class="icon_time"><a href="#">2015-1-9</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li>
+        <div class="ci"></div><!--圆形-->
+        <div class="row_box">
+            <div class="ti"></div><!--三角形-->
+
+            <h2 class="title"> <a href="#">我希望爱情是这样的</a></h2>
+            <ul class="textinfo">
+                <a href="#"><img src="images/s1.jpg"></a>
+                <p>我希望我的爱情是这样的，相濡以沫，举案齐眉，平淡如水。我在岁月中找到他，依靠他，将一生交付给他。做他的妻子，他孩子的母亲，为他做饭，洗衣服，缝一颗掉了的纽扣。然后，我们一起在时光中变老。</p>
+            </ul>
+            <ul class="details">
+                <li class="likes"><a href="#">10</a></li>
+                <li class="comments"><a href="#">34</a> </li>
+                <li class="icon_time"><a href="#">2015-1-9</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li>
+        <div class="row_box">
+            <div class="ti"></div><!--三角形-->
+            <div class="ci"></div><!--圆形-->
+            <h2 class="title"> <a href="#">我希望爱情是这样的</a></h2>
+            <ul class="textinfo">
+                <a href="#"><img src="images/s1.jpg"></a>
+                <p>我希望我的爱情是这样的，相濡以沫，举案齐眉，平淡如水。我在岁月中找到他，依靠他，将一生交付给他。做他的妻子，他孩子的母亲，为他做饭，洗衣服，缝一颗掉了的纽扣。然后，我们一起在时光中变老。</p>
+            </ul>
+            <ul class="details">
+                <li class="likes"><a href="#">10</a></li>
+                <li class="comments"><a href="#">34</a> </li>
+                <li class="icon_time"><a href="#">2015-1-9</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li>
+        <div class="row_box">
+            <div class="ti"></div><!--三角形-->
+            <div class="ci"></div><!--圆形-->
+            <h2 class="title"> <a href="#">我希望爱情是这样的</a></h2>
+            <ul class="textinfo">
+                <a href="#"><img src="images/s1.jpg"></a>
+                <p>我希望我的爱情是这样的，相濡以沫，举案齐眉，平淡如水。我在岁月中找到他，依靠他，将一生交付给他。做他的妻子，他孩子的母亲，为他做饭，洗衣服，缝一颗掉了的纽扣。然后，我们一起在时光中变老。</p>
+            </ul>
+            <ul class="details">
+                <li class="likes"><a href="#">10</a></li>
+                <li class="comments"><a href="#">34</a> </li>
+                <li class="icon_time"><a href="#">2015-1-9</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li>
+        <div class="row_box">
+            <div class="ti"></div><!--三角形-->
+            <div class="ci"></div><!--圆形-->
+            <h2 class="title"> <a href="#">我希望爱情是这样的</a></h2>
+            <ul class="textinfo">
+                <a href="#"><img src="images/s1.jpg"></a>
+                <p>我希望我的爱情是这样的，相濡以沫，举案齐眉，平淡如水。我在岁月中找到他，依靠他，将一生交付给他。做他的妻子，他孩子的母亲，为他做饭，洗衣服，缝一颗掉了的纽扣。然后，我们一起在时光中变老。</p>
+            </ul>
+            <ul class="details">
+                <li class="likes"><a href="#">10</a></li>
+                <li class="comments"><a href="#">34</a> </li>
+                <li class="icon_time"><a href="#">2015-1-9</a></li>
+            </ul>
+        </div>
+    </li>
+</ul>
