70da7c8fb03f0101e07f03adf3fd7eaa102c14e2
李立纯 <Fakeronline@users.noreply.github.com>
2015-07-31 22:29:06 +0800

    新一版前台模版

---
+++ resources/views/flatVersion/public/header.blade.php
@@ -0,0 +1,39 @@
+<div class="docs-header">
+    <!--nav-->
+    <nav class="navbar navbar-default navbar-custom" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target=".navbar-collapse">
+                    <span class="sr-only">Fakeronline</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" style="height: 1px;">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="nav-link current" href="getting-started.html">首页</a></li>
+                    <li><a class="nav-link" href="getting-started.html">文章类型</a></li>
+                    <li><a class="nav-link" href="documentation.html">随笔</a></li>
+                    <li><a class="nav-link" href="free-psd.html">留言</a></li>
+                    <li><a class="nav-link" href="color-picker.html">关于我的</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--header-->
+    <div class="topic">
+        <div class="container">
+            <div class="col-md-12">
+                <h3>Fakeronline</h3>
+                <h4>菜鸟不会飞~ 的个人博客</h4>
+            </div>
+        </div>
+        <div class="topic__infos">
+            <div class="container">
+                人如果没有梦想，那跟咸鱼有什么区别？ ———— 周星驰
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/flatVersion/public/layout.blade.php
@@ -0,0 +1,170 @@
+@section('hack_header')
+@show
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    @section('meta')
+    @show
+    <title>Fakeronline blog</title>
+    <link rel="stylesheet" href="{{_package('Bootflat/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{_package('Bootflat/bootflat/css/bootflat.min.css')}}">
+    <link rel="stylesheet" href="{{_package('Bootflat/css/site.min.css')}}">
+    <link rel="stylesheet" href="{{_asset('css/common.css')}}">
+    @section('link')
+    @show
+</head>
+
+<body>
+@section('bodyBefore')
+@show
+@include('flatVersion.public.header')
+<div class="main container">
+    <div class="col-lg-8">
+        @section('main')
+        @show
+    </div>
+    <div class="main-left col-lg-4">
+        <div class="panel">
+            <div class="panel-heading">
+                <h4 class="panel-title"><span class="glyphicon glyphicon-fire"></span>热门文章</h4>
+            </div>
+            <div class="panel-body rounded-list">
+                <ol>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                </ol>
+            </div>
+            <div class="panel-footer">
+                <a href="###">查看更多</a>
+            </div>
+
+        </div>
+
+        <div class="panel">
+
+            <div class="panel-heading">
+                <h4 class="panel-title"><span class="glyphicon glyphicon-fire"></span>热门标签</h4>
+            </div>
+            <div class="panel-body">
+                <div class="tag">
+                    <a  href="####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label label-default">ASP.NET</span></a>
+                    <a href="#####"><span class="label label-primary">JAVA</span></a>
+                    <a href="#####"><span class="label label-success">javascript</span></a>
+                    <a href="#####"><span class="label label-info">HTML</span></a>
+                    <a href="#####"><span class="label label-warning">CSS</span></a>
+                    <a href="#####"><span class="label label-danger">Jquery</span></a>
+                    <a  href="####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label label-default">ASP.NET</span></a>
+                    <a href="#####"><span class="label label-primary">JAVA</span></a>
+                    <a href="#####"><span class="label label-success">javascript</span></a>
+                    <a href="#####"><span class="label label-info">HTML</span></a>
+                    <a href="#####"><span class="label label-warning">CSS</span></a>
+                    <a href="#####"><span class="label label-danger">Jquery</span></a>
+                    <a  href="####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label">PHP</span></a>
+                    <a href="#####"><span class="label label-default">ASP.NET</span></a>
+                    <a href="#####"><span class="label label-primary">JAVA</span></a>
+                    <a href="#####"><span class="label label-success">javascript</span></a>
+                    <a href="#####"><span class="label label-info">HTML</span></a>
+                    <a href="#####"><span class="label label-warning">CSS</span></a>
+                    <a href="#####"><span class="label label-danger">Jquery</span></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel">
+
+            <div class="panel-heading">
+                <h4 class="panel-title"><span class="glyphicon glyphicon-thumbs-up"></span>推荐文章</h4>
+            </div>
+            <div class="panel-body rounded-list">
+                <ol>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                    <li><a href="###">PHP类的继承与多态</a></li>
+                </ol>
+            </div>
+            <div class="panel-footer">
+                <a href="###">查看更多</a>
+            </div>
+        </div>
+
+        <div class="panel">
+
+            <div class="panel-heading">
+                <h4 class="panel-title"><span class="glyphicon glyphicon-comment"></span>最新评论</h4>
+            </div>
+            <div class="panel-body">
+                <ul class="media-list">
+                    <li class="media">
+                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32" src="{{_asset('images/hd_pic.jpg')}}"></a>
+                        <div class="media-body">
+                            <h3 class="media-heading">PHP问题纠正</h3>
+                            <p>12 Apr, 2013 at 12:00</p>
+                            <p>如果可以再多爱一点，我可以去屎.</p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32" src="{{_asset('images/hd_pic.jpg')}}"></a>
+                        <div class="media-body">
+                            <h3 class="media-heading">PHP问题纠正</h3>
+                            <p>12 Apr, 2013 at 12:00</p>
+                            <p>如果可以再多爱一点，我可以去屎.</p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32" src="{{_asset('images/hd_pic.jpg')}}"></a>
+                        <div class="media-body">
+                            <h3 class="media-heading">PHP问题纠正</h3>
+                            <p>12 Apr, 2013 at 12:00</p>
+                            <p>如果可以再多爱一点，我可以去屎.</p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32" src="{{_asset('images/hd_pic.jpg')}}"></a>
+                        <div class="media-body">
+                            <h3 class="media-heading">PHP问题纠正</h3>
+                            <p>12 Apr, 2013 at 12:00</p>
+                            <p>如果可以再多爱一点，我可以去屎.</p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32" src="{{_asset('images/hd_pic.jpg')}}"></a>
+                        <div class="media-body">
+                            <h3 class="media-heading">PHP问题纠正</h3>
+                            <p>12 Apr, 2013 at 12:00</p>
+                            <p>如果可以再多爱一点，我可以去屎.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="panel-footer">
+                <a href="###">查看更多</a>
+            </div>
+        </div>
+    </div>
+</div>
+@include('flatVersion.public.footer')
+<!-- Bootstrap -->
+<script src="{{_package('Bootflat/js/jquery-1.10.1.min.js')}}"></script>
+<script src="{{_package('Bootflat/js/bootstrap.min.js')}}"></script>
+
+<!-- Bootflat's JS files.-->
+<script src="{{_package('Bootflat/bootflat/js/icheck.min.js')}}"></script>
+<script src="{{_package('Bootflat/bootflat/js/jquery.fs.selecter.min.js')}}"></script>
+<script src="{{_package('Bootflat/bootflat/js/jquery.fs.stepper.min.js')}}"></script>
+
+<scirpt src="{{_asset('js/common.js')}}"></scirpt>
+@section('bodyAfter')
+@show
+</body>
+</html>
