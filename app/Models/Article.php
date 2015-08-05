276212c7810494fdb4776f1e9779308b13fe8581
李立纯 <Fakeronline@users.noreply.github.com>
2015-08-05 21:35:14 +0800

    创建文章模型、标签模型、文章标签关系模型

---
+++ app/Models/Article.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Article.php
+ *
+ * Part of blog.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @author    Fackeronline <1077341744@qq.com>
+ * @link      https://github.com/Fakeronline
+ */
+ 
+namespace App\Models;
+
+class Article extends BaseModel{
+
+    protected $table = 'articles';
+    protected $guarded = [];    //支持所有批量更新
+}
