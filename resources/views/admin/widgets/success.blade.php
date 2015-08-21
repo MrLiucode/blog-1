4bd2b64d0e3fb56673020a5e2b951dcf8c11390a
Fackeronline <1077341744@qq.com>
2015-08-21 20:20:27 +0800

    后台文章、分类小修改

--- resources/views/admin/widgets/success.blade.php
+++ resources/views/admin/widgets/success.blade.php
@@ -1,5 +1,6 @@
 @if(Session::has('message'))
     <div class="alert alert-info">
         {{Session::pull('message')}}
+        <a href="#" data-dismiss="alert" class="close">×</a>
     </div>
 @endif
