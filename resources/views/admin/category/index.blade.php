162c9559eb69046c533ed547b57a9b7468fe4018
Fackeronline <1077341744@qq.com>
2015-09-30 20:13:07 +0800

    小修改

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -31,12 +31,12 @@
                 <div class="panel-heading">
                     <div class="panel-heading-btn">
                         <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
-                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-primary" data-click="panel-edit"><i class="fa fa-edit"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-primary" data-click="panel-edit"><i class="fa fa-plus"></i></a>
                         <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
                         <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
                         <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
                     </div>
-                    <h4 class="panel-title">文章列表</h4>
+                    <h4 class="panel-title">分类列表</h4>
                 </div>
                 <div class="panel-body">
                     <div class="table-responsive">
@@ -46,6 +46,7 @@
                                 <th></th>
                                 <th>分类名称</th>
                                 <th>描述</th>
+                                <th>添加者</th>
                                 <th>修改时间</th>
                                 <th>添加时间</th>
                                 <th></th>
@@ -56,33 +57,36 @@
                                     <td><span class="badge badge-primary">1</span></td>
                                     <td> laravel </td>
                                     <td> 记录有关Laravel的文章 </td>
+                                    <td> admin </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td style="width: 170px;;">
-                                        <button class="btn btn-primary btn-sm"><span class="fa  fa-edit"></span> 编辑 </button>
-                                        <button class="btn btn-danger btn-sm"><span class="fa fa-trash-o"></span> 删除 </button>
+                                        <button class="btn btn-primary btn-xs"><span class="fa  fa-edit"></span> 编辑 </button>
+                                        <button class="btn btn-danger btn-xs"><span class="fa fa-trash-o"></span> 删除 </button>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td><span class="badge badge-primary">2</span></td>
                                     <td> laravel </td>
                                     <td> 记录有关Laravel的文章 </td>
+                                    <td> admin </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td style="width: 170px;;">
-                                        <button class="btn btn-primary btn-sm"><span class="fa  fa-edit"></span> 编辑 </button>
-                                        <button class="btn btn-danger btn-sm"><span class="fa fa-trash-o"></span> 删除 </button>
+                                        <button class="btn btn-primary btn-xs"><span class="fa  fa-edit"></span> 编辑 </button>
+                                        <button class="btn btn-danger btn-xs"><span class="fa fa-trash-o"></span> 删除 </button>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td><span class="badge badge-primary">3</span></td>
                                     <td> laravel </td>
                                     <td> 记录有关Laravel的文章 </td>
+                                    <td> admin </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td> 2015-0715 12:01:12 </td>
                                     <td style="width: 170px;;">
-                                        <button class="btn btn-primary btn-sm"><span class="fa  fa-edit"></span> 编辑 </button>
-                                        <button class="btn btn-danger btn-sm"><span class="fa fa-trash-o"></span> 删除 </button>
+                                        <button class="btn btn-primary btn-xs"><span class="fa  fa-edit"></span> 编辑 </button>
+                                        <button class="btn btn-danger btn-xs"><span class="fa fa-trash-o"></span> 删除 </button>
                                     </td>
                                 </tr>
                             </tbody>
