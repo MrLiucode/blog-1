429103f609f28e1a0b0f35c7af31adb38a0d0028
lilichun <1077341744@qq.com>
2015-10-25 10:31:45 +0800

    update auth Provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use Auth;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +28,21 @@ class AuthServiceProvider extends ServiceProvider
     {
         parent::registerPolicies($gate);
 
-        //
+        $gate->define('route-permission', function($user, $routeName) {
+            $permitted = false;
+            $user->load('groups', 'groups.permissions');    //预载入权限组和权限列表
+            foreach($user->groups as $group) {
+                if($group->permissions->find($routeName)) {
+                    $permitted = true;
+                    break;
+                }
+            }
+            return $permitted;
+        });
+
+        $gate->define('check-super-permission', function($user){
+            return $user->email == config('auth.admin_user');
+        });
+
     }
 }
