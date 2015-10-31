0e38902b65c2283b3ed55ab8b5e0dc00dc90cf4c
Fackeronline <1077341744@qq.com>
2015-10-31 10:50:53 +0800

    文章模型结构修改

--- database/migrations/2015_08_29_082249_create_article_table.php
+++ database/migrations/2015_08_29_082249_create_article_table.php
@@ -18,12 +18,13 @@ class CreateArticleTable extends Migration
             $table->increments('id');
             $table->string('title', 140);   //标题
             $table->text('content');    //内容
-            $table->integer('click_num')->unsigned();  //点击次数
+            $table->integer('hits')->unsigned();  //点击次数
             $table->integer('is_top')->default(0)->unsigned();  //是否置顶
             $table->integer('status')->default(1)->unsigned();  //状态[0=不发布，1=发布]
             $table->integer('user_id')->unsigned();    //添加用户
-            $table->integer('created_at');
-            $table->integer('updated_at');
+            $table->integer('published_at')->unsigned();    //发布时间
+            $table->integer('created_at')->unsigned();
+            $table->integer('updated_at')->unsigned();
             $table->foreign('user_id')
                 ->references('id')->on('users')
                 ->onUpdate('cascade')
