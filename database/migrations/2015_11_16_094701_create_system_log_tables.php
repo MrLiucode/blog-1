7f07d2afa14424e1b97b9ec7637297dd7cd14d8e
lilichun <1077341744@qq.com>
2015-11-16 17:30:32 +0800

    系统错误日志UML

---
+++ database/migrations/2015_11_16_094701_create_system_log_tables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemLogTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('error_logs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+
+            $table->string('file_name')->index();    //文件名称(带路径)
+            $table->string('type'); //错误类型
+            $table->string('message');  //错误消息
+            $table->text('trace_message'); //详细信息
+
+            $table->text('headers');    //header头信息
+            $table->string('http_method', 50)->index();  //请求方式
+            $table->string('ip')->index();  //IP地址
+            $table->string('request_url');  //请求的URL
+            $table->string('user_agent');   //浏览器标识
+
+            $table->integer('user_id'); //用户ID
+
+            $table->integer('created_at')->unsigned();
+            $table->integer('updated_at')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('error_logs');
+    }
+}
