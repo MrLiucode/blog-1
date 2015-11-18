39c0ce4c61b550205c15b57c13a64784e93a3d10
lilichun <1077341744@qq.com>
2015-11-18 13:24:36 +0800

    异常处理过滤路由错误

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,6 +18,7 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         HttpException::class,
         ModelNotFoundException::class,
+        NotFoundHttpException::class,
     ];
 
     /**
