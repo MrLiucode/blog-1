d0ae8f3b17150a4f5edaf8f90e2189fc4e66628a
lilichun <1077341744@qq.com>
2015-11-20 15:46:06 +0800

    Error Log Model

--- app/Models/ErrorLog.php
+++ app/Models/ErrorLog.php
@@ -4,5 +4,12 @@ namespace App\Models;
 
 class ErrorLog extends BaseModel
 {
+
+    protected $with = ['user'];
+
     protected $table = 'error_logs';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
