12709677dcdadfc1e00cf580b37c5c804f78b5dd
lilichun <1077341744@qq.com>
2015-11-20 15:47:36 +0800

    Error Log View

---
+++ resources/views/admin/error_log/index.blade.php
@@ -0,0 +1,92 @@
+@extends('admin.layout.base')
+
+@section('title')系统错误日志@stop
+
+@section('breadcrumb')
+    <ol class="breadcrumb pull-right">
+        <li><a href="javascript:;">首页</a></li>
+        <li><a href="javascript:;">系统错误日志</a></li>
+        <li class="active">错误日志列表</li>
+    </ol>
+@stop
+
+@section('page-header')
+    错误日志列表
+    @stop
+
+    @section('css')
+    {!! createConcat('admin/plugins/', [
+        'DataTables/css/data-table.css',
+    ]) !!}
+    @stop
+
+
+    @section('content')
+            <!-- begin row -->
+    <div class="row">
+        <!-- begin col-12 -->
+        <div class="col-md-12">
+            <!-- begin panel -->
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                    </div>
+                    <h4 class="panel-title">错误日志列表</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table id="data-table" class="table table-striped table-bordered">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>错误消息</th>
+                                <th>错误类型</th>
+                                <th>链接地址</th>
+                                <th width="80px;">请求方式</th>
+                                <th width="80px;">严重级别</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($errorLogList as $index => $item)
+                            <tr>
+                                <td><span class="badge badge-primary">{{$index + 1}}</span></td>
+                                <td> <a class="u-block" href="{{route('admin.errorLog.show', ['id' => $item->id])}}" target="_blank"><strong>{{$item->message}}</strong>  [<small>{{$item->type}}</small>]
+                                    <br /> <small>触发时间: {!! $item->created_at->diffForHumans() !!}</small> </a> </td>
+                                <td> {{$item->type}} </td>
+                                <td><a class="u-block" href="{{$item->request_url}}" target="_blank">{{$item->request_url}}</a></td>
+                                <td> {{$item->http_method}} </td>
+                                <td style="width: 170px;;">
+                                    {!! $item->severity ? '<span class="badge badge-danger">错误</span>' : '<span class="badge badge-warning">警告</span>' !!}
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="dataTables_paginate paging_simple_numbers" id="data-table_paginate">
+                            {!! $errorLogList->render() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end panel -->
+        </div>
+        <!-- end col-12 -->
+    </div>
+@stop
+
+@section('js')
+    {!! createConcat('admin', [
+        'plugins/DataTables/js/jquery.dataTables.js',
+        'js/apps.min.js',
+        'js/dashboard-v2.min.js'
+        ])
+     !!}
+
+    <script>
+        App.init();
+    </script>
+@stop
---
+++ resources/views/admin/error_log/show.blade.php
@@ -0,0 +1,127 @@
+@extends('admin.layout.base')
+
+@section('title')系统错误日志@stop
+
+@section('breadcrumb')
+    <ol class="breadcrumb pull-right">
+        <li><a href="javascript:;">首页</a></li>
+        <li><a href="{{route('admin.errorLog.index')}}">系统错误日志</a></li>
+        <li class="active">错误日志明细</li>
+    </ol>
+@stop
+
+@section('page-header')
+    错误日志明细
+    @stop
+
+    @section('css')
+    {!! createConcat('admin/plugins/', [
+        'DataTables/css/data-table.css',
+    ]) !!}
+    @stop
+
+
+    @section('content')
+            <!-- begin row -->
+    <div class="row">
+        <!-- begin col-12 -->
+        <div class="col-md-12">
+            <!-- begin panel -->
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                    </div>
+                    <h4 class="panel-title">错误日志列表</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="m-t-10"> {{$errorLog->type}} <small> {{$errorLog->http_method}} / <a
+                                            href="{{$errorLog->request_url}}" target="_blank">{{$errorLog->request_url}}</a> </small> </h3>
+                        </div>
+                        <!-- begin col-6 -->
+                        <div class="col-md-12">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a href="#default-tab-1" data-toggle="tab">消息</a></li>
+                                <li class=""><a href="#default-tab-2" data-toggle="tab">请求</a></li>
+                                <li class=""><a href="#default-tab-3" data-toggle="tab">用户信息</a></li>
+                            </ul>
+                            <div class="tab-content">
+                                <div class="tab-pane fade active in" id="default-tab-1">
+                                    <h3 class="m-t-10"> {{$errorLog->message}} </h3><hr>
+                                    <p>{!! handelTraceString($errorLog->trace_message) !!}</p>
+                                </div>
+                                <div class="tab-pane fade" id="default-tab-2">
+
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"><h4>URL:</h4></span>
+                                        <div class="col-md-8">
+                                            <pre> <a href="{{$errorLog->request_url}}" target="_blank">{{$errorLog->request_url}}</a> </pre>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"><h4>请求方式:</h4></span>
+                                        <div class="col-md-8">
+                                            <pre> {{$errorLog->http_method}}</pre>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"><h4>IP地址:</h4></span>
+                                        <div class="col-md-8">
+                                            <pre> {{$errorLog->ip}}</pre>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"><h4>浏览器标识:</h4></span>
+                                        <div class="col-md-8">
+                                            <pre> {{$errorLog->user_agent}}</pre>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"> <h4>headers头:</h4> </span>
+                                        <div class="col-md-8">
+                                            <pre>
+                                                {!! print_r(json_decode($errorLog->headers, true)) !!}
+                                            </pre>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="tab-pane fade" id="default-tab-3">
+                                    <div class="row">
+                                        <span class="col-md-1 tx-r"> <h4>用户邮箱:</h4> </span>
+                                        <span class="col-md-1 tx-r"> <h5>{{$errorLog->user->email ?: '未知用户' }}</h5> </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
+                </div>
+            </div>
+            <!-- end panel -->
+        </div>
+        <!-- end col-12 -->
+    </div>
+@stop
+
+@section('js')
+    {!! createConcat('admin', [
+        'plugins/DataTables/js/jquery.dataTables.js',
+        'js/apps.min.js',
+        'js/dashboard-v2.min.js'
+        ])
+     !!}
+
+    <script>
+        App.init();
+    </script>
+@stop
