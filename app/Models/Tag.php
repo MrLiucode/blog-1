fbeadad7bd3de3dd3025b3282eb4d0bf320a11a1
lilichun <1077341744@qq.com>
2016-01-21 11:47:23 +0800

    tag model update

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,16 +9,27 @@
  * @author    Fackeronline <1077341744@qq.com>
  * @link      https://github.com/Fakeronline
  */
- 
+
 namespace App\Models;
 
-class Tag extends BaseModel{
+class Tag extends BaseModel
+{
 
     protected $table = 'tags';
 
     public function article()
     {
         return $this->belongsToMany(Article::class);
+    }
+
+    public function scopeTagName($query, array $tagName = [])
+    {
+        return $query->whereIn('name', $tagName);
+    }
+
+    public function scopeTagId($query, $tagId)
+    {
+        return $query->where('id', $tagId);
     }
 
 }
