ea41fe372bf2a53b4f54a33905c245e50c7d691d
lilichun <1077341744@qq.com>
2016-01-21 15:20:38 +0800

    Category Model update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -16,8 +16,12 @@ class Category extends BaseModel{
 
     protected $table = 'categories';
 
+    /**
+     * 获取该标签下的所有文章
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function article()
     {
-        return $this->belongsToMany(Article::class);
+        return $this->belongsToMany(Article::class);    //一个文章分类下有多个文章
     }
 }
