20af2bb1709714de0ff8db2828ab7e331a21fa2b
lilichun <1077341744@qq.com>
2016-01-22 14:02:20 +0800

    update Article Interface

--- app/Contracts/IArticle.php
+++ app/Contracts/IArticle.php
@@ -1,8 +1,36 @@
 <?php namespace App\Contracts;
 
 use App\Http\Requests\ArticleRequest;
+use App\Models\Article as ArticleModel;
 
 interface IArticle
 {
+    /**
+     * 保存文章
+     * @param ArticleRequest $request
+     * @return ArticleModel|null
+     */
     public function store(ArticleRequest $request);
+
+    /**
+     * 修改文章
+     * @param ArticleModel $article
+     * @param ArticleRequest $request
+     * @return int|null
+     */
+    public function update(ArticleModel $article, ArticleRequest $request);
+
+    /**
+     * 删除文章
+     * @param ArticleModel $article
+     * @return int
+     */
+    public function destroy(ArticleModel $article);
+
+    /**
+     * 获取错误消息
+     * @return string|null
+     */
+    public function getError();
+
 }
