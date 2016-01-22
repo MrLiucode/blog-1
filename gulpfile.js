e390f935dfc82976480957bff84c00dc27d78077
lilichun <1077341744@qq.com>
2016-01-22 16:19:30 +0800

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var elixir = require('laravel-elixir');
 var resourcesDir = 'resources/assets';
 var publicCssDir = 'public/static/css';
 elixir(function (mix) {
-    mix.copy(resourcesDir + '/package', 'public/static/package');
-    mix.copy(resourcesDir + '/js/admin', 'public/static/js/admin');
-    mix.copy(resourcesDir + '/css/admin', 'public/static/css/admin');
+    mix.copy(resourcesDir + '/package', 'public/static/package');   //移动包文件
+    mix.copy(resourcesDir + '/js/admin', 'public/static/js/admin'); //移动后台JS文件
+    mix.copy(resourcesDir + '/css/admin', 'public/static/css/admin');   //移动后台CSS文件
+    mix.copy(resourcesDir + '/images', 'public/static/images');  //移动前后端素材图片
 
     //首页css
     mix.styles([
