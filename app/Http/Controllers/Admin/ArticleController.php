fc145a7fc2f485a43649b8dd349aaa371dff2f8a
lilichun <1077341744@qq.com>
2016-01-22 14:05:33 +0800

    update Article Controller

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -9,17 +9,56 @@
  * @author    Fackeronline <1077341744@qq.com>
  * @link      https://github.com/Fakeronline
  */
- 
+
 namespace App\Http\Controllers\Admin;
 
-class ArticleController extends BaseController{
+use App\Contracts\IArticle;
+use App\Contracts\ICategory;
+use App\Contracts\ITag;
+use App\Http\Requests\ArticleRequest;
+use App\Models\Article as ArticleModel;
 
-    public function index(){
-        return adminView('article.index');
+class ArticleController extends BaseController
+{
+
+    const VIEW_INDEX = 'article.index';
+    const VIEW_EDIT = 'article.edit';
+    const ROUTE_INDEX = 'admin.article.index';
+
+    public function index()
+    {
+        return adminView(self::VIEW_INDEX);
     }
 
-    public function create(){
-        return adminView('article.edit');
+    public function create(ICategory $category, ITag $tag)
+    {
+        $categoryList = array_key_value($category->lists()->toArray()['data'], 'id', 'name'); //获取所有的分类
+        $tagList = array_key_value($tag->getList(9999)->toArray()['data'], 'id', 'name'); //获取所有的标签
+        return adminView('article.edit', compact('categoryList', 'tagList'));
+    }
+
+    public function store(ArticleRequest $request, IArticle $articleService)
+    {
+        $result = $articleService->store($request);
+        return $result ? success(route(self::ROUTE_INDEX)) : error($articleService->getError() ?: '创建文章失败!');
+    }
+
+    public function edit(ArticleModel $article)
+    {
+        return adminView(self::VIEW_EDIT, compact('article'));
+    }
+
+
+    public function update(ArticleModel $article, ArticleRequest $request, IArticle $articleService)
+    {
+        $result = $articleService->update($article, $request);
+        return $result ? success(route(self::ROUTE_INDEX)) : error($articleService->getError() ?: '修改文章失败!');
+    }
+
+    public function destroy(ArticleModel $article, IArticle $articleService)
+    {
+        $result = $articleService->destroy($article);
+        return $result ? success(route(self::ROUTE_INDEX)) : error($articleService->getError() ?: '删除文章失败!');
     }
 
 }
