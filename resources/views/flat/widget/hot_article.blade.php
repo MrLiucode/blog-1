a14b64619d0b0536fcb71e905670cc3223d571d5
Fackeronline <1077341744@qq.com>
2016-01-23 13:08:29 +0800

    create widget view

---
+++ resources/views/flat/widget/hot_article.blade.php
@@ -0,0 +1,19 @@
+<div class="panel">
+
+    <div class="panel-heading">
+        <h4 class="panel-title"><span class="glyphicon glyphicon-fire"></span>热门文章</h4>
+    </div>
+    <div class="panel-body rounded-list">
+        <ol>
+            <li><a href="###">PHP类的继承与多态</a></li>
+            <li><a href="###">PHP类的继承与多态</a></li>
+            <li><a href="###">PHP类的继承与多态</a></li>
+            <li><a href="###">PHP类的继承与多态</a></li>
+            <li><a href="###">PHP类的继承与多态</a></li>
+        </ol>
+    </div>
+    <div class="panel-footer">
+        <a href="###">查看更多</a>
+    </div>
+
+</div>
