f09843178a520002a2b42a9656fdf2650e1b8746
Fackeronline <1077341744@qq.com>
2016-01-23 13:07:58 +0800

    create layout view

--- resources/views/flat/layout/base.blade.php
+++ resources/views/flat/layout/base.blade.php
@@ -1,165 +1,36 @@
-@section('hack_header')
-@show
-        <!doctype html>
+<!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8"/>
-    @section('meta')
-    @show
-    <title>首页@section('title')@show</title>
-    <meta name="keywords" content="Fakeronline"/>
-    <meta name="description" content="Fakeronline"/>
-    <meta name="author" content="Fakeronline"/>
-    {!! createConcat('static/package/bootflat', [
-        'bootstrap/bootstrap.min.css', 'css/bootflat.css'
+    <meta charset="UTF-8" />
+    <base href="blog.com">
+    <title>Fakeronline blog</title>
+    {!! createConcat('static/package/Bootflat', [
+        'css/bootstrap.min.css', 'bootflat/css/bootflat.min.css', 'css/site.min.css'
     ]) !!}
     {!! createConcat('static/css', [
         'common.css'
     ]) !!}
-    {!! createConcat('static/package/bootflat/js', [
-        'jquery-1.10.1.min.js'
+    {!! createConcat('static/', [
+        'package/Bootflat/js/jquery-1.10.1.min.js', 'js/common.js'
     ]) !!}
-    @section('link')
-    @show
+    @section('css') @show
 </head>
 
 <body>
-@section('bodyBefore')
-@show
+
 @include('flat.layout.header')
-<div class="main container">
-    <div class="col-lg-8">
-        @section('main')
-        @show
-    </div>
-    <div class="main-left col-lg-4">
-        <div class="panel">
-            <div class="panel-heading">
-                <h4 class="panel-title"><span class="glyphicon glyphicon-fire"></span>热门文章</h4>
-            </div>
-            <div class="panel-body rounded-list">
-                <ol>
-                    @foreach($hotArticle as $item)
-                        <li><a href="###">{{$item->title}}</a></li>
-                    @endforeach
-                </ol>
-            </div>
-            <div class="panel-footer">
-                <a href="###">查看更多</a>
-            </div>
 
-        </div>
-
-        <div class="panel">
-
-            <div class="panel-heading">
-                <h4 class="panel-title"><span class="glyphicon glyphicon-fire"></span>热门标签</h4>
-            </div>
-            <div class="panel-body">
-                <div class="tag">
-                    @foreach($hotTagList as $item)
-                        <a href="####"><span class="label {!! getRandClass() !!} ">{{$item['name']}}</span></a>
-                    @endforeach
-                    @if(empty($hotTagList))
-                        暂没有任何标签
-                    @endif
-                    &nbsp;
-                </div>
-            </div>
-        </div>
-
-        <div class="panel">
-
-            <div class="panel-heading">
-                <h4 class="panel-title"><span class="glyphicon glyphicon-thumbs-up"></span>推荐文章</h4>
-            </div>
-            <div class="panel-body rounded-list">
-                <ol>
-                    @foreach($goodArticle as $item)
-                        <li><a href="###">{{$item->title}}</a></li>
-                    @endforeach
-                </ol>
-            </div>
-            <div class="panel-footer">
-                <a href="###">查看更多</a>
-            </div>
-        </div>
-
-        <div class="panel">
-
-            <div class="panel-heading">
-                <h4 class="panel-title"><span class="glyphicon glyphicon-comment"></span>最新评论</h4>
-            </div>
-            <div class="panel-body">
-                <ul class="media-list">
-                    <li class="media">
-                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32"
-                                                           src="{{asset('static/images/hd_pic.jpg')}}"></a>
-                        <div class="media-body">
-                            <h3 class="media-heading">PHP问题纠正</h3>
-                            <p>12 Apr, 2013 at 12:00</p>
-                            <p>如果可以再多爱一点，我可以去屎.</p>
-                        </div>
-                    </li>
-                    <li class="media">
-                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32"
-                                                           src="{{asset('static/images/hd_pic.jpg')}}"></a>
-                        <div class="media-body">
-                            <h3 class="media-heading">PHP问题纠正</h3>
-                            <p>12 Apr, 2013 at 12:00</p>
-                            <p>如果可以再多爱一点，我可以去屎.</p>
-                        </div>
-                    </li>
-                    <li class="media">
-                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32"
-                                                           src="{{asset('static/images/hd_pic.jpg')}}"></a>
-                        <div class="media-body">
-                            <h3 class="media-heading">PHP问题纠正</h3>
-                            <p>12 Apr, 2013 at 12:00</p>
-                            <p>如果可以再多爱一点，我可以去屎.</p>
-                        </div>
-                    </li>
-                    <li class="media">
-                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32"
-                                                           src="{{asset('static/images/hd_pic.jpg')}}"></a>
-                        <div class="media-body">
-                            <h3 class="media-heading">PHP问题纠正</h3>
-                            <p>12 Apr, 2013 at 12:00</p>
-                            <p>如果可以再多爱一点，我可以去屎.</p>
-                        </div>
-                    </li>
-                    <li class="media">
-                        <a class="pull-left" href="#"><img class="media-object img-rounded head-pic-32"
-                                                           src="{{asset('static/images/hd_pic.jpg')}}"></a>
-                        <div class="media-body">
-                            <h3 class="media-heading">PHP问题纠正</h3>
-                            <p>12 Apr, 2013 at 12:00</p>
-                            <p>如果可以再多爱一点，我可以去屎.</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div class="panel-footer">
-                <a href="###">查看更多</a>
-            </div>
-        </div>
-    </div>
-</div>
-@include('flat.layout.footer')
-        <!-- Bootstrap -->
-{!! createConcat('static/package/bootflat/js', [
-    'bootstrap.min.js', 'jquery.icheck.js'
-]) !!}
-
-        <!--[if IE 8]>
-{!! createConcat('static/package/bootflat/js', [
-    'respond.min.js', 'html5shiv.js'
-]) !!}
-        <!--[if !IE]><!-->
-{!! createConcat('static/js', [
-    'common.js'
-]) !!}
-@section('bodyAfter')
+@section('container')
 @show
+
+@include('flat.layout.footer')
+{!! createConcat('static/package/Bootflat', [
+    'js/bootstrap.min.js', 'bootflat/js/icheck.min.js',
+    'bootflat/js/jquery.fs.selecter.min.js',
+    'bootflat/js/jquery.fs.stepper.min.js'
+]) !!}
+
+@section('js') @show
+
 </body>
 </html>
--- resources/views/flat/layout/footer.blade.php
+++ resources/views/flat/layout/footer.blade.php
@@ -1,8 +1,43 @@
 <div class="footer">
     <div class="container">
+        <div class="clearfix">
+            <dl class="footer-nav">
+                <dt class="nav-title">文章分类</dt>
+                <dd class="nav-item"><a href="#">PHP</a></dd>
+                <dd class="nav-item"><a href="#">jAVA</a></dd>
+                <dd class="nav-item"><a href="#">.NET</a></dd>
+                <dd class="nav-item"><a href="#">HTML</a></dd>
+                <dd class="nav-item"><a href="#">CSS</a></dd>
+            </dl>
+            <dl class="footer-nav">
+                <dt class="nav-title">文章分类</dt>
+                <dd class="nav-item"><a href="#">PHP</a></dd>
+                <dd class="nav-item"><a href="#">jAVA</a></dd>
+                <dd class="nav-item"><a href="#">.NET</a></dd>
+                <dd class="nav-item"><a href="#">HTML</a></dd>
+                <dd class="nav-item"><a href="#">CSS</a></dd>
+            </dl>
+            <dl class="footer-nav">
+                <dt class="nav-title">文章分类</dt>
+                <dd class="nav-item"><a href="#">PHP</a></dd>
+                <dd class="nav-item"><a href="#">jAVA</a></dd>
+                <dd class="nav-item"><a href="#">.NET</a></dd>
+                <dd class="nav-item"><a href="#">HTML</a></dd>
+                <dd class="nav-item"><a href="#">CSS</a></dd>
+            </dl>
+            <dl class="footer-nav">
+                <dt class="nav-title">文章分类</dt>
+                <dd class="nav-item"><a href="#">PHP</a></dd>
+                <dd class="nav-item"><a href="#">jAVA</a></dd>
+                <dd class="nav-item"><a href="#">.NET</a></dd>
+                <dd class="nav-item"><a href="#">HTML</a></dd>
+                <dd class="nav-item"><a href="#">CSS</a></dd>
+            </dl>
+        </div>
         <div class="footer-copyright text-center">
-            {!! array_get($optionList, 'website_copy', '') !!}<br />
-            {!! array_get($optionList, 'website_footer', '') !!}
+            Copyright © 2015 Fakeronline 菜鸟不会飞~
+            <a href="https://github.com/fakeronline" target="_blank">Github</a>&nbsp;|
+            <a href="https://packagist.org/packages/fakeronline/" target="_blank">Packagist</a>
         </div>
     </div>
 </div>
