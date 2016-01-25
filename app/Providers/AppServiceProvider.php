249242644a0770fb5fbe76c7bfec05cb43f5c533
lilichun <1077341744@qq.com>
2016-01-25 18:01:07 +0800

    move interface binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\ViewData;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,58 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->bindingInterface();  //绑定接口
+        $this->bindFacade();    //绑定外观
     }
+
+    protected function bindingInterface()
+    {
+        //绑定权限服务
+        $this->app->bind(
+            \App\Contracts\IACLPermission::class,
+            \App\Services\ACLPermission::class
+        );
+
+        //绑定权限组服务
+        $this->app->bind(
+            \App\Contracts\IACLGroup::class,
+            \App\Services\ACLGroup::class
+        );
+
+        //绑定用户管理服务
+        $this->app->bind(
+            \App\Contracts\IACLUser::class,
+            \App\Services\ACLUser::class
+        );
+
+        $this->app->bind(
+            \App\Contracts\IErrorLog::class,
+            \App\Services\ErrorLog::class
+        );
+
+        //文章分类接口绑定
+        $this->app->bind(
+            \App\Contracts\ICategory::class,
+            \App\Services\Category::class
+        );
+
+        //标签接口绑定
+        $this->app->bind(
+            \App\Contracts\ITag::class,
+            \App\Services\Tag::class
+        );
+
+        $this->app->bind(
+            \App\Contracts\IArticle::class,
+            \App\Services\Article::class
+        );
+    }
+
+    public function bindFacade()
+    {
+        $this->app->bind('ViewData', function () {
+            return new ViewData();
+        });
+    }
+
 }
