53314cbfe1d05444456d2a233bf58c43c7efb56c
lilichun <1077341744@qq.com>
2016-01-28 10:48:54 +0800

    static file update

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,10 @@ var resourcesDir = 'resources/assets';
 var publicCssDir = 'public/static/css';
 var publicJsDir = 'public/static/js';
 elixir(function (mix) {
-    mix.copy(resourcesDir + '/package', 'public/static/package');   //移动包文件
     mix.copy(resourcesDir + '/js/admin', 'public/static/js/admin'); //移动后台JS文件
     mix.copy(resourcesDir + '/css/admin', 'public/static/css/admin');   //移动后台CSS文件
     mix.copy(resourcesDir + '/images', 'public/static/images');  //移动前后端素材图片
-
+    mix.copy(resourcesDir + '/fonts', 'public/static/fonts');  //移动字体文件
     //首页css
     mix.styles([
         resourcesDir + '/css/components/collection.css',
